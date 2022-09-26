17e5251dd54de21c62c1a706efe9e61d796635ee
Andres <andrespuntofinal@gmail.com>
2022-09-26 11:14:27 -0500

    docker

--- BackCRUD/Program.cs
+++ BackCRUD/Program.cs
@@ -21,6 +21,7 @@ namespace BackCRUD
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:8084");
                 });
     }
 }
